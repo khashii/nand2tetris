c164a419e70cd8a41d826fb8664a3566c5aea821
khashi <k.hashi0624@gmail.com>
2019-11-08 23:21:23 +0900

    implement 07 stage#2

--- projects/07/VMtranslator/codeWriter.js
+++ projects/07/VMtranslator/codeWriter.js
@@ -1,14 +1,13 @@
 const fs = require("fs");
 
 const _FILE_NAME = Symbol();
+const _FILE_PATH = Symbol();
 const _RESULT = Symbol();
-const _STACK_INDEX = Symbol();
 
 module.exports = class CodeWriter {
 
   constructor() {
     this[_RESULT] = "";
-    this[_STACK_INDEX] = 255;
     this.eqCnt = 0;
     this.gtCnt = 0;
     this.ltCnt = 0;
@@ -17,10 +16,11 @@ module.exports = class CodeWriter {
   /**
    * CodeWriteモジュールに新しいVMファイルの変換が
    * 開始したことを知らせる
-   * @param {string} fileName
+   * @param {string} filePath
    */
-  setFileName(fileName) {
-    this[_FILE_NAME] = fileName;
+  setFileName(filePath) {
+    this[_FILE_NAME] = filePath.split("/").pop().replace(/.vm/, "");
+    this[_FILE_PATH] = filePath;
   }
 
   /**
@@ -29,41 +29,42 @@ module.exports = class CodeWriter {
    * @param {string} command
    */
   writeArithmetic(command) {
+    this[_RESULT] += `// ${command}\n`;
     switch (command) {
       case "add":
-        this[_RESULT] += `@${this[_STACK_INDEX]}\nD=M\n@${--this[_STACK_INDEX]}\nM=D+M\n@SP\nM=M-1\n`;
+        this[_RESULT] += `@SP\nAM=M-1\nD=M\n@SP\nAM=M-1\nM=D+M\n@SP\nM=M+1\n`;
         break;
 
       case "sub":
-        this[_RESULT] += `@${this[_STACK_INDEX]}\nD=M\n@${--this[_STACK_INDEX]}\nM=M-D\n@SP\nM=M-1\n`;
+        this[_RESULT] += `@SP\nAM=M-1\nD=M\n@SP\nAM=M-1\nM=M-D\n@SP\nM=M+1\n`;
         break;
 
       case "neg":
-        this[_RESULT] += `@${this[_STACK_INDEX]}\nM=-M\n`;
+        this[_RESULT] += `@SP\nA=M-1\nM=-M\n`;
         break;
 
       case "eq":
-        this[_RESULT] += `@${this[_STACK_INDEX]}\nD=M\n@${--this[_STACK_INDEX]}\nD=D-M\nM=0\n@EQ${++this.eqCnt}\nD;JEQ\n@SKIP_EQ${this.eqCnt}\n0;JMP\n(EQ${this.eqCnt})\n@${this[_STACK_INDEX]}\nM=-1\n(SKIP_EQ${this.eqCnt})\n@SP\nM=M-1\n`;
+        this[_RESULT] += `@SP\nAM=M-1\nD=M\n@SP\nAM=M-1\nD=M-D\n@EQ${++this.eqCnt}\nD;JEQ\n@SP\nA=M\nM=0\n@SKIP_EQ${this.eqCnt}\n0;JMP\n(EQ${this.eqCnt})\n@SP\nA=M\nM=-1\n(SKIP_EQ${this.eqCnt})\n@SP\nM=M+1\n`;
         break;
 
       case "gt":
-        this[_RESULT] += `@${this[_STACK_INDEX]}\nD=M\n@${--this[_STACK_INDEX]}\nD=M-D\nM=0\n@GT${++this.gtCnt}\nD;JGT\n@SKIP_GT${this.gtCnt}\n0;JMP\n(GT${this.gtCnt})\n@${this[_STACK_INDEX]}\nM=-1\n(SKIP_GT${this.gtCnt})\n@SP\nM=M-1\n`;
+        this[_RESULT] += `@SP\nAM=M-1\nD=M\n@SP\nAM=M-1\nD=M-D\n@GT${++this.gtCnt}\nD;JGT\n@SP\nA=M\nM=0\n@SKIP_GT${this.gtCnt}\n0;JMP\n(GT${this.gtCnt})\n@SP\nA=M\nM=-1\n(SKIP_GT${this.gtCnt})\n@SP\nM=M+1\n`;
         break;
 
       case "lt":
-        this[_RESULT] += `@${this[_STACK_INDEX]}\nD=M\n@${--this[_STACK_INDEX]}\nD=M-D\nM=0\n@LT${++this.ltCnt}\nD;JLT\n@SKIP_LT${this.ltCnt}\n0;JMP\n(LT${this.ltCnt})\n@${this[_STACK_INDEX]}\nM=-1\n(SKIP_LT${this.ltCnt})\n@SP\nM=M-1\n`;
+        this[_RESULT] += `@SP\nAM=M-1\nD=M\n@SP\nAM=M-1\nD=M-D\n@LT${++this.ltCnt}\nD;JLT\n@SP\nA=M\nM=0\n@SKIP_LT${this.ltCnt}\n0;JMP\n(LT${this.ltCnt})\n@SP\nA=M\nM=-1\n(SKIP_LT${this.ltCnt})\n@SP\nM=M+1\n`;
         break;
 
       case "and":
-        this[_RESULT] += `@${this[_STACK_INDEX]}\nD=M\n@${--this[_STACK_INDEX]}\nM=D&M\n@SP\nM=M-1\n`;
+        this[_RESULT] += `@SP\nAM=M-1\nD=M\n@SP\nAM=M-1\nM=D&M\n@SP\nM=M+1\n`;
         break;
 
       case "or":
-        this[_RESULT] += `@${this[_STACK_INDEX]}\nD=M\n@${--this[_STACK_INDEX]}\nM=D|M\n@SP\nM=M-1\n`;
+        this[_RESULT] += `@SP\nAM=M-1\nD=M\n@SP\nAM=M-1\nM=D|M\n@SP\nM=M+1\n`;
         break;
 
       case "not":
-        this[_RESULT] += `@${this[_STACK_INDEX]}\nM=!M\n`;
+        this[_RESULT] += `@SP\nA=M-1\nM=!M\n`;
         break;
 
       default:
@@ -80,24 +81,71 @@ module.exports = class CodeWriter {
    * @param {number} index >= 0
    */
   writePushPop(command, segment, index) {
+    this[_RESULT] += `// ${command} ${segment} ${index}\n`;
     if (command === "C_PUSH") {
-      this[_STACK_INDEX]++;
       switch (segment) {
         case "constant":
-          this[_RESULT] += `@${index}\nD=A\n@${this[_STACK_INDEX]}\nM=D\n@SP\nM=M+1\n`;
+          this[_RESULT] += `@${index}\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n`;
+          break;
+        case "local":
+          this[_RESULT] += `@${index}\nD=A\n@LCL\nA=M+D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n`;
+          break;
+        case "argument":
+          this[_RESULT] += `@${index}\nD=A\n@ARG\nA=M+D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n`;
+          break;
+        case "this":
+          this[_RESULT] += `@${index}\nD=A\n@THIS\nA=M+D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n`;
+          break;
+        case "that":
+          this[_RESULT] += `@${index}\nD=A\n@THAT\nA=M+D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n`;
+          break;
+        case "pointer":
+          this[_RESULT] += `@${3+index}\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n`;
+          break;
+        case "temp":
+          this[_RESULT] += `@${5+index}\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n`;
+          break;
+        case "static":
+          this[_RESULT] += `@${this[_FILE_NAME]}.${index}\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n`;
           break;
         default:
           break;
       }
     } else if (command === "C_POP") {
-      // ステージ2で実装する
+      switch (segment) {
+        case "constant":
+          break;
+        case "local":
+          this[_RESULT] += `@${index}\nD=A\n@LCL\nD=M+D\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n`;
+          break;
+        case "argument":
+          this[_RESULT] += `@${index}\nD=A\n@ARG\nD=M+D\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n`;
+          break;
+        case "this":
+          this[_RESULT] += `@${index}\nD=A\n@THIS\nD=M+D\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n`;
+          break;
+        case "that":
+          this[_RESULT] += `@${index}\nD=A\n@THAT\nD=M+D\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n`;
+          break;
+        case "pointer":
+          this[_RESULT] += `@SP\nAM=M-1\nD=M\n@${3+index}\nM=D\n`;
+          break;
+        case "temp":
+          this[_RESULT] += `@SP\nAM=M-1\nD=M\n@${5+index}\nM=D\n`;
+          break;
+        case "static":
+          this[_RESULT] += `@SP\nAM=M-1\nD=M\n@${this[_FILE_NAME]}.${index}\nM=D\n`;
+          break;
+        default:
+          break;
+      }
     }
 
   }
 
   close() {
     // 雑
-    fs.writeFileSync(this[_FILE_NAME].replace(/.vm/, ".asm"), this[_RESULT]);
+    fs.writeFileSync(this[_FILE_PATH].replace(/.vm/, ".asm"), this[_RESULT]);
   }
 
 }
